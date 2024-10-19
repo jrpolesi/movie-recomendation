ed0a0bdaf81f1b806a499edf8216aa89ee916ff1
jose ricardo kremer polesi <josericardo.kremer@gmail.com>
2024-10-19 16:51:13 -0300

    feat: add navigation to header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,40 @@
+import { NavLink } from "react-router-dom";
 import { DarkModeBtn } from "../DarkModeBtn";
 import styles from "./style.module.css";
+
+const ROUTES = [
+  {
+    label: "Filmes",
+    to: "/movies",
+  },
+  {
+    label: "Séries",
+    to: "/series",
+  },
+];
 
 export function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.headerContainer}>
         <h1>MyMovies</h1>
+
+        <nav>
+          <ul className={styles.navList}>
+            {ROUTES.map(({ label, to }) => (
+              <li key={label}>
+                <NavLink
+                  to={to}
+                  className={({ isActive }) =>
+                    isActive ? styles.activeLink : ""
+                  }
+                >
+                  {label}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+        </nav>
 
         <DarkModeBtn />
       </div>
